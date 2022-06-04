2eb3e4b10d04074c405c1628bd8d4b21a4cf9cb4
SergeyBakunin <serge.bakunin@gmail.com>
2022-06-04 04:50:19 +0300

    Add project files.

---
+++ HW_6/HW_6.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_6
+{
+    class HW_6
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Список всех процессов: ");
+            var processList = Process.GetProcesses();
+            foreach (var process in processList)
+            {
+                Console.WriteLine($"{process.Id} {process.ProcessName}");
+            }
+            Console.Write("Название процесса: ");
+            var killName = Console.ReadLine();
+            try
+            {
+                processList.First(p => p.ProcessName.ToLower() == killName.ToLower()).Kill();
+                Console.WriteLine($"{killName} завершен!");
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine($"Процесс {killName} не найден!");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
